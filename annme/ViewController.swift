496d464de79b592d52031a68cf469aea6f846427
annafkh <annafi101@gmail.com>
2025-04-17 15:22:44 +0700

    Initial Commit

---
+++ annme/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  annme
+//
+//  Created by Muhammad Annafi Fakhruddin on 2025-04-17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ annmeTests/annmeTests.swift
@@ -0,0 +1,17 @@
+//
+//  annmeTests.swift
+//  annmeTests
+//
+//  Created by Muhammad Annafi Fakhruddin on 2025-04-17.
+//
+
+import Testing
+@testable import annme
+
+struct annmeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
